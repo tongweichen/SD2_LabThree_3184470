a08a5dcbee25636815dcd2d43d6b061cba9cdc11
dangdang <dangdang@172.19.11.129>
2026-03-04 17:43:44 +0000

    implement circle

---
+++ src/griffith/Circle.java
@@ -0,0 +1,34 @@
+package griffith;
+
+public class Circle extends Shape {
+
+    private double radius;
+
+    public Circle(String name, double radius) {
+        super(name);
+        this.radius = radius;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public double area() {
+        return Math.PI * radius * radius;
+    }
+
+    @Override
+    public double perimeter() {
+        return 2 * Math.PI * radius;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + " radius=" + radius;
+    }
+}
